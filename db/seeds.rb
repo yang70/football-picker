bf043818efd2ddad4348d8bc2fac5e8bc5dd6892
Matthew Yang <yang70@gmail.com>
2015-11-22 01:00:36 -0800

    remove poltergeist from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
 require 'capybara'
-require 'capybara/poltergeist'
 
 include Capybara::DSL
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
-end
-
-Capybara.default_driver = :poltergeist
 
 (1..17).each do |num|
   Week.create!(week: num)
