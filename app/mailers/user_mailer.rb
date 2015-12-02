79259cbe2670f911ae8461d9f25473bb3b9afc3c
Matthew Yang <yang70@gmail.com>
2015-12-01 23:36:23 -0800

    fix syntax error in usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ApplicationMailer
     @user = user
     @week = week
     @weekly_score = @user.weekly_score.where(week_id: @week).score
-    mail(to: @user.email, subject: "Football Picker - Week #{@week} results"
+    mail(to: @user.email, subject: "Football Picker - Week #{@week} results")
   end
 end
